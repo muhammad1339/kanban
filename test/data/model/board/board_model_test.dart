8c5a7cb92df3dc05708f12bf33fc661b8a268261
abo-el30la <muhammadahmed1339@gmail.com>
2023-03-03 13:31:59 +0200

    update:(first test horray)

--- test/data/model/board/board_model_test.dart
+++ test/data/model/board/board_model_test.dart
@@ -3,9 +3,10 @@ import 'package:kanban/data/model/model.dart';
 
 void main() {
   test('board nodel test', () {
-    final board = Board(name: "board 1", id: "123");
+    final board = Board(name: "board 1", id: "123",taskCards: []);
     expect(board.id, "123");
     expect(board.name, "board 1");
+    expect(board.taskCards, []);
 
   });
 }
